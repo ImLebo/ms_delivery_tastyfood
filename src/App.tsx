4625271201be925f3add8b5bbf08f217b0a3f838
Sakuga-05 <sakuga.09@gmail.com>
2025-05-19 00:41:17 -0500

    Implementación del login con Google

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import routes from './routes';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const CLIENTE_ID = "828480505438-lfssskehmilcnc0fv3q9hrjls1v1oh5m.apps.googleusercontent.com"
 
 function App() {
-
-
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          {routes.map((routes, index) => {
-            const {path, component: Component} = routes;
-            return(
-            <Route
-              key={index}
-              path={path}
-              element={
-                <Component></Component>
-              }
-            >
+    <GoogleOAuthProvider clientId={CLIENTE_ID}>
+      <Router>
+        <div className="app">
+          <Routes>
+            {routes.map((routes, index) => {
+              const {path, component: Component} = routes;
+              return(
+              <Route
+                key={index}
+                path={path}
+                element={
+                  <Component></Component>
+                }
+              >
 
-            </Route>);
-          })}
-        </Routes>
-      </div>
-    </Router>
+              </Route>);
+            })}
+          </Routes>
+        </div>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
