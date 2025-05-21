04ada4347fe34145bfc0a292a45f082e1811a1b3
Sakuga-05 <sakuga.09@gmail.com>
2025-05-21 04:40:26 -0500

    Vistas de gestionar producto y gestionar restaurante terminadas con backend conectado. En proceso la de gestionar menú. Se agregaron iconos nuevos y una paleta de color en  tailwind config

--- src/pages/Restaurante/GestionarMenu.tsx
+++ src/pages/Restaurante/GestionarMenu.tsx
@@ -24,7 +24,37 @@ const GestionarMenu: React.FC = () => {
                     <div className="w-full h-px bg-azul-principal my-2"></div>
                 </div>
 
-            </div>
+                <div className="flex flex-col justify-center">
+                    <div className="max-w-sm p-3 bg-pastelCyan rounded-xl shadow-md border border-gray-200 relative">
+                        {/* Botón cerrar */}
+                        <button className="absolute top-3 right-3 text-gray-500 hover:text-gray-700 text-xl">
+                            <span className="w-8 h-8 md:w-9 md:h-9 inline-block cancel-icon"></span>
+                        </button>
+
+                        <button className="top-3 right-3 text-gray-700 hover:text-cyan-500 text-xl">
+                            <span className="w-8 h-8 md:w-9 md:h-9 inline-block editar-icon"></span>
+                        </button>
+
+                        {/* Imagen */}
+                        <div className="w-28 h-28 overflow-hidden rounded-full mx-auto mb-4">
+                            <img
+                            src="/src/images/imagen-plato-generica-2.png"
+                            alt="Plato de pasta"
+                            className="object-cover w-full h-full"
+                            />
+                        </div>
+
+                        <div className="w-full h-[60%] flex flex-row p-4">
+                            <div className=" flex flex-col justify-center pr-[5rem] md:pr-[5rem]">
+                                <p className="inline-block font-semibold text-lg text-left">Nombre: </p>
+                                <p className="inline-block font-semibold text-lg text-left">Dirección: </p>
+                                <p className="inline-block font-semibold text-lg text-left">Teléfono: </p>
+                                <p className="inline-block font-semibold text-lg text-left">Correo: </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> {/*div final*/}
 
             <div className="">
                 <NavMovil informacion={InfoSinNav}></NavMovil>
